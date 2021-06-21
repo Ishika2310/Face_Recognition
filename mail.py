7c628374c82382fcc92e20e4320d335ec16d5304
Ishika2310 <66568005+Ishika2310@users.noreply.github.com>
2021-06-21 22:52:37 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,10 +1,10 @@
 import smtplib as s
 ob=s.SMTP("smtp.gmail.com",587)
 ob.starttls()
-ob.login("ishika.sharma2310@gmail.com","IshikaSharma2310")
+ob.login("sender_mail_address","user_password")
 subject="Sending email using python"
 body="Hey ! Hope you are doing well and keeping safe ! Have a great day ahead ! Thanks ..."
 message="subject:{}\n\n{}".format(subject,body)
-ob.sendmail("ishika.sharma2310@gmail.com","ishash2310@gmail.com",message)
+ob.sendmail("sender_mail_address","receiver_mail_address",message)
 print("Sent successfully..")
 ob.quit()
